2735718024a0c03943bf6f375ce1173a021703fe
Ernesto Neyra <ernestoneyra31@gmail.com>
2022-01-31 09:56:40 +0100

    added a backend

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+const data = {
+  school: [
+    {
+      name: "Lanekalkylator",
+      description:
+        "En kalkylator för att räkna ut kostnaden per månad, tänta och totala kostnaden baserat på ens summa, ränta och återbetalningsperiod. Den är skapad i endast HTML, CSS och JavaScript och var examinationen för JavaScript kursen.",
+      image: "../images/Portfolio/lanekalkylator.jpg",
+      link: "https://github.com/ernestoneyra/JSExam",
+    },
+    {
+      name: "Instaclone",
+      description:
+        "En React app med Firebase som backend med Bootstrap. Det vara första gången som jag använde Firebase och enda. Det är en väldigt simpel version av Instagram och fungerar på samma sätt. Den vare examinationen för ReactJS kursen.",
+      image: "../images/Portfolio/socialapp.jpg",
+      link: "https://github.com/ernestoneyra/social-media-app/tree/main/social-media-app",
+    },
+    {
+      name: "Amaclon",
+      description:
+        "En Amazon klon som är byggd med MERN stacken. Funktionerna är demsamma som Amazon, men väldigt förenklade. Det var första gången som jag använde en backend. Jag valde MongodDB efetersom jag kunde använda ett GUI och det verkade vara mer populärt.",
+      image: "../images//Portfolio/amaclon.jpg",
+      link: "https://github.com/ernestoneyra/AmaClon/tree/deploy",
+    },
+    {
+      name: "Highscore list",
+      description:
+        "En simpel highscore lista som vilket spel, senaste poäng, top poäng och vem som har poäng. Skapad med MERN stacken. Denna var examinationen för backend kursen i NodeJS.",
+        image: '../images/Portfolio/highscore.jpg',
+        link: 'https://github.com/ernestoneyra/highscore'
+    },
+    {
+        name: 'Kontakts',
+        description: 'En kontaktlista app som är gjord med MERN stacken. Har samtliga CRUD funktioner. Var en av 2 examinationer i SCRUM kursen. Eftresom fokus på kursen var att kunna arbeta agilt i en grupp med SCRUM är appen väldigt enkel.',
+        image: '../images/Portfolio/kontakts.jpg',
+        link: 'https://github.com/ali-Alrubaye/scrum_group3'
+    },
+    {
+        name: 'Hitta en bok',
+        description: 'En liten application för bibliotek där man får en lista med antingen fysiska böcker eller ljudböcker. Jag gjorde den här i vår CMS kurs där vi lärde oss om Strapi och dess funktioner.',
+        image: '../images/Portfolio/böcker.jpg',
+        link: 'https://github.com/ernestoneyra/bibliotek-app/tree/master'
+    }
+  ],
+};
+
+module.exports = data 
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const pastWorkRouter = require("./routes/pastworkRouter");
+require('dotenv').config()
+
+
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  
+}, () => (
+  console.log('connected to database')
+));
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('Open', () => console.log('Connected to database'))
+
+app.use(express.json())
+
+
+app.use('/pastwork/school', pastWorkRouter)
+
+app.listen(3000, () => console.log("Server Started on 3000"));
+
